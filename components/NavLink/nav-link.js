896316b0d4be63f42368223218725c609e7fe0b2
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2026-01-19 00:18:58 +0530

    Using the client components efficiently by making them as leaf components

---
+++ components/NavLink/nav-link.js
@@ -0,0 +1,19 @@
+'use client';
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import styles from './nav-link.module.css';
+
+export const NavLink = ({ href, children }) => {
+  const path = usePathname();
+
+  return (
+    <Link
+      href={href}
+      className={`${styles.link} ${path.startsWith(href) ?  styles.active : ''}`}
+    >
+      {children}
+    </Link>
+  );
+}
