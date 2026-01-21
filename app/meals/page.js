9270eebc84eb797f7aed8d3a419bb11aa580f846
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2026-01-21 20:21:56 +0530

    Add meal components and database initialization
    - Created CSS module for meal item styling.
    - Implemented MealsGrid component to display a list of meals.
    - Added CSS module for meals grid layout.
    - Initialized SQLite database with dummy meal data.
    - Created a meals library to fetch meals from the database.
    - Updated package.json and package-lock.json to include better-sqlite3 dependency.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,29 @@
 import Link from "next/link";
 
-export default function MealsPage() {
+import styles from './page.module.css';
+import { MealsGrid } from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+
+  const meals = await getMeals();
+
   return (
-    <main>
-      <h1>Meals Page</h1>
-      <Link href="/meals/share">Share Page</Link>
-      <br />
-      <Link href="/meals/dynamic">Dynamic Page</Link>
-    </main>
-  )
+    <>
+      <header className={ styles.header}>
+        <h1>Delicious meals, created {' '}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">
+            Share Your Favorite Recipe
+          </Link>
+        </p>
+      </header>
+      <main>
+        <MealsGrid meals={meals}/>
+      </main>
+    </>
+  );
 }
