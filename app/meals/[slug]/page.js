9c3a4c9332d0aad49b3e3660bf57e1a7ae8da68e
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2026-02-10 23:45:59 +0530

    Implement MealDetailPage component with styles; add meal details and image display

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,31 @@
-export default async function DynamicMealsRoute({params}) {
+import Image from 'next/image';
+import styles from './page.module.css';
+import { getMeal } from '@/lib/meals';
+
+export default async function MealDetailPage({ params }) {
   const { slug } = await params;
+  const { title, image, summary, instructions, creator, creator_email } = getMeal(slug);
 
   return (
-    <main>
-      <h1>Dynamic route - {slug}</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={image} fill/>
+        </div>
+        <div className={styles.headerText}>
+          <h1>{title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={styles.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={styles.instructions} dangerouslySetInnerHTML={{
+          __html: instructions.replace(/\n/g, '<br />')
+        }}>
+        </p>
+      </main>
+    </>
   )
 }
